0aef74179ed7f71c4b390ce30171bb4c76ba09cc
Flovenice <iphonya4@gmail.com>
2025-02-12 22:13:31 +0300

    Добавление и получение закупок (по одной или все)

--- server/controllers/purchaseController.js
+++ server/controllers/purchaseController.js
@@ -4,20 +4,27 @@ import { Purchase } from '../models/models.js';
 class PurchaseController{
     async create(req, res, next) {
         try {
-            let {id, purchaser, item, number, price, law, link, method, expire_date, platform, application_securing, contract_securing, contact} = req.body;
+            const {id, purchaser, item, number, price, law, link, method, expire_date, platform, application_securing, contract_securing, contact} = req.body;
             const purchase = await Purchase.create({id, purchaser, item, number, price, law, link, method, expire_date, platform, application_securing, contract_securing, contact});
             return res.json(purchase);
         } catch (e) {
-            return next(ApiError.badRequest(e.message));
+            next(ApiError.badRequest(e.message));
         }
     }
 
     async getAll(req, res) {
-
+        let {limit, page} = req.query;
+        page = page || 1;
+        limit = limit || 10;
+        let offset = page * limit - limit;
+        let purchases = await Purchase.findAll({limit, offset});
+        return res.json(purchases);
     }
 
     async getOne(req, res) {
-
+        const {id} = req.params;
+        const purchase = await Purchase.findOne({where: {id}});
+        return res.json(purchase);
     }
 }
 
--- server/routes/purchaseRouter.js
+++ server/routes/purchaseRouter.js
@@ -4,5 +4,6 @@ const router = new Router();
 
 router.post('/', purchaseController.create);
 router.get('/', purchaseController.getAll);
+router.get('/:id', purchaseController.getOne);
 
 export default router;
