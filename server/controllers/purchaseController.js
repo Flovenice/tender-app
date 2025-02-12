e73c28ae68d92353e47001447385c21a8cee69bf
Flovenice <iphonya4@gmail.com>
2025-02-12 15:29:21 +0300

    Регистрация пользователя, JWT токен

--- server/controllers/purchaseController.js
+++ server/controllers/purchaseController.js
@@ -1,8 +1,15 @@
+import ApiError from '../error/ApiError.js';
 import { Purchase } from '../models/models.js';
 
 class PurchaseController{
-    async create(req, res) {
-
+    async create(req, res, next) {
+        try {
+            const {id, purchaser, item, number, price, law, link, method, expire_date, platform, application_securing, contract_securing, contact} = req.body;
+            const purchase = await Purchase.create({id, purchaser, item, number, price, law, link, method, expire_date, platform, application_securing, contract_securing, contact});
+            return res.json(purchase);
+        } catch (e) {
+            return next(ApiError.badRequest(e.message));
+        }
     }
 
     async getAll(req, res) {
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,27 @@
 import ApiError from "../error/ApiError.js";
+import { Purchase, User } from '../models/models.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const generateJwt = function(id, email, role) {
+    return jwt.sign({id, email, role}, process.env.SECRET_KEY, {expiresIn: '24h'});
+}
 
 class UserController{
-    async registration(req, res) {
-        const {email, password, role} = req.query;
-        
+    async registration(req, res, next) {
+        const {email, password, role} = req.body;
+        if (!email || !password) {
+            return next(ApiError.badRequest('Неверный email или пароль'));
+        }
+        const candidate = await User.findOne({where: {email}});
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь уже существует'));
+        }
+        const hashedPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({email, password: hashedPassword, role});
+        const token = generateJwt(user.id, user.email, user.role);
+
+        return res.json({token});
     }
 
     async login(req, res) {
